511143877f54eb191f54e72da2d2bb37cb18470d
GAYIVORE <gayivore@gmail.com>
2026-05-12 16:57:05 +0000

    fix: explicit reset of git file casing for Vercel

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,9 @@ import { Toaster } from "@/components/ui/toaster"
 import { QueryClientProvider } from '@tanstack/react-query'
 import { queryClientInstance } from '@/lib/query-client'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import { AuthProvider } from '@/lib/AuthProvider.js'; 
+import { AuthProvider } from '@/lib/AuthProvider.jsx'; 
 import Home from '@/pages/Home';
-import PageNotFound from '@/lib/PageNotFound.js'; 
+import PageNotFound from '@/lib/PageNotFound.jsx'; 
 
 function App() {
   return (
